cb9c91ac73fd14937c1be6159872cb808da63bea
Femi <bolajifemi28@gmail.com>
2020-06-30 22:59:42 +0100

    added top margin to contact page send button

--- src/styles/contactStyle.js
+++ src/styles/contactStyle.js
@@ -1,11 +1,12 @@
 const contactStyle = (theme) => ({
-	padding: {
-		padding: theme.spacing.unit * 2
-	},
-	button: {
-		'&:focus': {
-			backgroundColor: theme.palette.secondary.light
-		}
-	}
+  padding: {
+    padding: theme.spacing.unit * 2,
+  },
+  button: {
+    "&:focus": {
+      backgroundColor: theme.palette.secondary.light,
+    },
+    marginTop: theme.spacing(2),
+  },
 });
 export default contactStyle;
